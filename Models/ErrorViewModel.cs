12e4fb44707161a57a9cec2623969c9523474afc
gamer99122 <gamer99122@hotmail.com.tw>
2024-12-06 17:25:41 +0800

    add first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
